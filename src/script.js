ff3ef3c7aef12ca1e73bcb97e6e7ce61b0792067
Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
2017-04-29 04:33:28 +0200

    Calculate max width size for each font.

--- src/script.js
+++ src/script.js
@@ -6,3 +6,19 @@
  *  @copyright  NhuVo
  */
 
+var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '*', '%', '/', '\'', '"', '?', '^', '!', '-', '_', ',', ';', '.', ':', '<', '>', '\\'];
+var fonts = ["DISPLAY FREE TFB", "Simple S", "Score Board", "monospace"];
+var sizes = [0, 0 , 0, 0];
+var character = document.getElementById("character");
+
+for(f = 0; f < fonts.length; f++) {
+  for(c = 0; c < characters.length; c++) {
+    character.style.fontFamily = fonts[f];
+    character.innerText = characters[c];
+    if(character.offsetWidth > sizes[f]) {
+      sizes[f] = character.offsetWidth;
+    }
+  }
+}
+
+character.style.display = "none";
