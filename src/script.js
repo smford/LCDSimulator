3dfbe35f261eac68d146ec8bb49399b04ab6a609
Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
2017-04-30 15:51:29 +0200

    Create html, css & javascript file (empty) using Bootstrap & jQuery.

---
+++ src/script.js
@@ -0,0 +1,8 @@
+/**
+ *  @file       script.js
+ *  @version    0.1
+ *  @date       2017-04-30
+ *  @author     VO, Nhu-Hoai Robert <nhuhoai.vo@franicflow.ch>
+ *  @copyright  FRANIC Flow Sàrl
+ *  @brief      Main scripts
+ */
